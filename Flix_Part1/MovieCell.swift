45a329047c5c4891b55e331526d8c3f1d75c5266
Haitao Huang <hht1996@hotmail.com>
2020-02-21 02:37:07 -0800

    Adding workspace

---
+++ Flix_Part1/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flix_Part1
+//
+//  Created by Haitao Huang on 2/21/20.
+//  Copyright © 2020 hht1996@hotmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
